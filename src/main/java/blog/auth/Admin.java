2e205756f4615cd0326f5dcd6ca47492f08e5f5a
Jgus <srraight@gmail.com>
2022-11-12 20:33:15 +0900

    remove PropertySource annotation from Admin & implements InitializingBean to Admin

--- src/main/java/blog/auth/Admin.java
+++ src/main/java/blog/auth/Admin.java
@@ -1,6 +1,7 @@
 package blog.auth;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,16 +11,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-@PropertySource("classpath:application.properties")
-public class Admin {
-    private final String id;
-    private final String pwd;
+public class Admin implements InitializingBean {
+    @Value("${id}")
+    private String id;
+    @Value("${password}")
+    private String pwd;
     private final PasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    @Autowired
-    public Admin(@Value("${id}") String id, @Value("${password}") String pwd){
-        this.id = id;
-        this.pwd = encoder.encode(pwd);
+    @Override
+    public void afterPropertiesSet() {
+        pwd = encoder.encode(pwd);
     }
     public boolean match(String id, String pwd){
         if(!id.equals(this.id)) return false;
--- src/main/java/blog/post/PostRepository.java
+++ src/main/java/blog/post/PostRepository.java
@@ -1,10 +1,10 @@
 package blog.post;
 
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.mongodb.repository.MongoRepository;
+        import org.springframework.stereotype.Repository;
+        import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
+        import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
